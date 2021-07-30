b4a84bb7f52a41c4f74d57cbc66ddecb93a13cad
troymeeker <troymeeker1@gmail.com>
2021-07-30 13:57:38 -0700

    complete

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your code here
+
+
+function submitData(name,email){
+  return fetch("http://localhost:3000/users",{
+  method:"POST",
+    headers:{
+      "Content-Type":"application/json",
+      "Accept":"application/json"
+    },
+    body:JSON.stringify({
+      name:name,
+      email:email
+    })
+  })
+  .then(res=>res.json())
+  .then(obj => document.body.innerHTML = obj.id)
+    .catch(error => document.body.innerHTML = error.message)
+}
+ 
